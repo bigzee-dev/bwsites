b916e9ceed9dcc8cb205371c4be77130a1792637
Kei Zimmermann <zimamankei@gmail.com>
2026-09-03 12:30:22 +0200

    tests

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { SitesGrid } from "@/components/sites-grid";
 import { CategoryLinks } from "@/components/client/category-links";
 import { SearchBanner } from "@/components/client/search-banner";
 import { CollectionsContainer } from "@/components/collections-container";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,6 +14,8 @@ export default function Home() {
         <Hero />
         <CollectionsContainer collections={[1, 2, 3]} />
         <CategoryLinks />
+        <a href="/search/media">PLAIN HTML TEST</a>
+        <Link href="/search/media">LINK TEST</Link>
         <CollectionsContainer collections={[4, 5, 6]} />
         <SearchBanner />
         <CollectionsContainer collections={[7, 8, 9]} />
